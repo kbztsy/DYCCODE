d6220c39b13a75ac9a5caf00298a7f9b80273178
孙伯天 <1085584851@qq.com>
2018-05-04 11:39:16 +0800

    增加艾滋和伤害Model

--- src/main/java/com/dtsp/demo/oldModel/Hurt.java
+++ src/main/java/com/dtsp/demo/oldModel/Hurt.java
@@ -1,4 +1,159 @@
 package com.dtsp.demo.oldModel;
 
+import java.util.Date;
+
 public class Hurt {
+    private String patient_name;
+    private String sex_id;
+    private String nationality_id;
+    private String hj;
+    private Date date_of_birth;
+    private String occupation_id;
+    private String address;
+    private String xzd;
+    private String sfgy;
+    private String xshbw;
+    private String fsdd;
+    private String yzcd;
+    private Date fs_date;
+    private Date date_of_tb;
+    private String tbdw;
+    private String person_of_tb;
+    private String dept_id;
+
+    public String getPatient_name() {
+        return patient_name;
+    }
+
+    public void setPatient_name(String patient_name) {
+        this.patient_name = patient_name;
+    }
+
+    public String getSex_id() {
+        return sex_id;
+    }
+
+    public void setSex_id(String sex_id) {
+        this.sex_id = sex_id;
+    }
+
+    public String getNationality_id() {
+        return nationality_id;
+    }
+
+    public void setNationality_id(String nationality_id) {
+        this.nationality_id = nationality_id;
+    }
+
+    public String getHj() {
+        return hj;
+    }
+
+    public void setHj(String hj) {
+        this.hj = hj;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getOccupation_id() {
+        return occupation_id;
+    }
+
+    public void setOccupation_id(String occupation_id) {
+        this.occupation_id = occupation_id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getXzd() {
+        return xzd;
+    }
+
+    public void setXzd(String xzd) {
+        this.xzd = xzd;
+    }
+
+    public String getSfgy() {
+        return sfgy;
+    }
+
+    public void setSfgy(String sfgy) {
+        this.sfgy = sfgy;
+    }
+
+    public String getXshbw() {
+        return xshbw;
+    }
+
+    public void setXshbw(String xshbw) {
+        this.xshbw = xshbw;
+    }
+
+    public String getFsdd() {
+        return fsdd;
+    }
+
+    public void setFsdd(String fsdd) {
+        this.fsdd = fsdd;
+    }
+
+    public String getYzcd() {
+        return yzcd;
+    }
+
+    public void setYzcd(String yzcd) {
+        this.yzcd = yzcd;
+    }
+
+    public Date getFs_date() {
+        return fs_date;
+    }
+
+    public void setFs_date(Date fs_date) {
+        this.fs_date = fs_date;
+    }
+
+    public Date getDate_of_tb() {
+        return date_of_tb;
+    }
+
+    public void setDate_of_tb(Date date_of_tb) {
+        this.date_of_tb = date_of_tb;
+    }
+
+    public String getTbdw() {
+        return tbdw;
+    }
+
+    public void setTbdw(String tbdw) {
+        this.tbdw = tbdw;
+    }
+
+    public String getPerson_of_tb() {
+        return person_of_tb;
+    }
+
+    public void setPerson_of_tb(String person_of_tb) {
+        this.person_of_tb = person_of_tb;
+    }
+
+    public String getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(String dept_id) {
+        this.dept_id = dept_id;
+    }
 }
