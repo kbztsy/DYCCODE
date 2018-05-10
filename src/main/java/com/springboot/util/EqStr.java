81ed63aeedcc075f1a10e493a009faeee2b62a64
田世岩 <425380330@qq.com>
2018-05-10 18:54:14 +0800

    吸烟转换

---
+++ src/main/java/com/springboot/util/EqStr.java
@@ -0,0 +1,21 @@
+package com.springboot.util;
+
+public class EqStr {
+    public int RefStr(String str){
+        int type=0;
+        switch (str){
+            case "吸烟":
+                type = 1;
+            break;
+            case "已戒烟":
+                type= 2;
+                break;
+            case "从不吸烟":
+                type= 3;
+                break;
+                default:
+                    type= 9;
+        }
+        return type;
+    }
+}
