0cda03ae81c86314995331cc78d70fa2b6c9151b
田世岩 <425380330@qq.com>
2018-05-11 16:41:57 +0800

    修改主键为纯字母数字

--- src/main/java/com/dtsp/util/RandString.java
+++ src/main/java/com/dtsp/util/RandString.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public class RandString {
     public String RandStr() {
-        String uuid = UUID.randomUUID().toString().substring(0,15);
+        String uuid = UUID.randomUUID().toString().replaceAll("-","").substring(0,15);
         return  uuid;
     }
 }
