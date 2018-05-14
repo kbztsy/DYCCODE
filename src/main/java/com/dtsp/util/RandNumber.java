b7874346bfb06de778f2384592cab2e6fce80666
田世岩 <425380330@qq.com>
2018-05-14 14:17:27 +0800

    增加log4J配置文件

--- src/main/java/com/dtsp/util/RandNumber.java
+++ src/main/java/com/dtsp/util/RandNumber.java
@@ -3,12 +3,26 @@ package com.dtsp.util;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class RandNumber {
-    public BigDecimal RandInt20(){
+    public String RandStr15(){
+        String uuid = UUID.randomUUID().toString().replaceAll("-","").substring(0,15);
+        return uuid;
+    }
+    public BigDecimal RandInt10(){
         String intStr = "";
-        for(int i=0;i<20;i++){
+        for(int i=0;i<10;i++){
+            int in = (int)(Math.random()*9);
+            intStr+=in;
+        }
+        return new BigDecimal(Double.parseDouble(intStr));
+    }
+    public BigDecimal RandInt20(){
+        String intStr = new Date().getTime()+"";
+        for(int i=0;i<7;i++){
             int in = (int)(Math.random()*9);
             intStr += in;
         }
